d045aa7b3585955545cfd009569ec72d76647fc2
whddus9509 <whddus9509@naver.com>
2020-01-10 14:17:28 +0900

    Repository Test 수정

--- spring-boot-project2(person)/mycontact/src/test/java/com/fastcampus/project2/mycontact/repository/PersonRepositoryTest.java
+++ spring-boot-project2(person)/mycontact/src/test/java/com/fastcampus/project2/mycontact/repository/PersonRepositoryTest.java
@@ -1,14 +1,17 @@
 package com.fastcampus.project2.mycontact.repository;
 
 import com.fastcampus.project2.mycontact.domain.Person;
+import com.fastcampus.project2.mycontact.domain.dto.Birthday;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @Transactional
 @SpringBootTest
@@ -16,29 +19,45 @@ class PersonRepositoryTest {
 
     @Autowired
     private PersonRepository personRepository;
-
     @Test
-    void crud(){
-       Person person=new Person();
-       person.setName("Jong");
+    void findByName(){
+        List<Person> people=personRepository.findByName("tony");
+        assertThat(people.size()).isEqualTo(1);
 
-       personRepository.save(person);
-        List<Person> result=personRepository.findByName("Jong");
-        assertThat(result.size()).isEqualTo(1);
-        assertThat(result.get(0).getName()).isEqualTo("Jong");
-//        assertThat(result.get(0).getAge()).isEqualTo(25);
+        Person person=people.get(0);
+        assertAll(
+                ()->assertThat(person.getName()).isEqualTo("tony"),
+                ()-> assertThat(person.getHobby()).isEqualTo("reading"),
+                ()-> assertThat(person.getJob()).isEqualTo("officer"),
+                ()->assertThat(person.getBirthday()).isEqualTo(Birthday.of(LocalDate.of(1991,7,10))),
+                ()->assertThat(person.getAddress()).isEqualTo("seoul"),
+                ()-> assertThat(person.getPhoneNumber()).isEqualTo("010-2222-5555"),
+                ()->assertThat(person.isDeleted()).isEqualTo(false)
+        );
     }
 
-
+    @Test
+    void findByNameIfDeleted(){
+        List<Person> people=personRepository.findByName("andrew");
+        assertThat(people.size()).isEqualTo(0);
+    }
 
     @Test
-    void findByBirthdayBetween(){
+    void findByMonthOfBirthday(){
+        List<Person> people= personRepository.findByMonthOfBirthday(7);
+        assertThat(people.size()).isEqualTo(3);
+        assertAll(
+                ()->assertThat(people.get(0).getName()).isEqualTo("david"),
+                ()->assertThat(people.get(1).getName()).isEqualTo("JongYeon"),
+                ()->assertThat(people.get(2).getName()).isEqualTo("tony")
+        );
+    }
 
-        List<Person> result=personRepository.findByMonthOfBirthday(8);
-        assertThat(result.size()).isEqualTo(2);
-        assertThat(result.get(0).getName()).isEqualTo("martin");
-        assertThat(result.get(1).getName()).isEqualTo("minwoo");
-
+    @Test
+    void  findPeopleDeleted(){
+        List<Person> people=personRepository.findPeopleDeleted();
+        assertThat(people.size()).isEqualTo(1);
+        assertThat(people.get(0).getName()).isEqualTo("andrew");
     }
 
 }
--- spring-boot-project2(person)/mycontact/src/test/resources/data.sql
+++ spring-boot-project2(person)/mycontact/src/test/resources/data.sql
@@ -2,4 +2,7 @@ insert into person(`id`,`name`,`year_of_birthday`,`month_of_birthday`,`day_of_bi
 insert into person(`id`,`name`,`year_of_birthday`,`month_of_birthday`,`day_of_birthday`)values (2,'david',1992,7,25);
 insert into person(`id`,`name`,`year_of_birthday`,`month_of_birthday`,`day_of_birthday`)values (3,'wonku',1995,10,27);
 insert into person(`id`,`name`,`year_of_birthday`,`month_of_birthday`,`day_of_birthday`)values (4,'minwoo',1995,8,27);
-insert into person(`id`,`name`,`year_of_birthday`,`month_of_birthday`,`day_of_birthday`)values (5,'JongYeon',81995,7,7);
+insert into person(`id`,`name`,`year_of_birthday`,`month_of_birthday`,`day_of_birthday`)values (5,'JongYeon',1995,7,7);
+insert into person(`id`,`name`,`year_of_birthday`,`month_of_birthday`,`day_of_birthday`,`job`,`hobby`,`phone_number`,`address`)
+    values(6,'tony',1991,7,10,'officer','reading','010-2222-5555','seoul');
+insert into person(`id`,`name`,`deleted`) values(7,'andrew',true);
