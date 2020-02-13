de79c54491349ee7b838d86071a7e1a3c8db6be2
whddus9509 <whddus9509@naver.com>
2020-02-13 16:11:45 +0900

    connect mysql

--- spring-boot-project3(admin)/src/main/java/com/example/study/model/SearchParam.java
+++ spring-boot-project3(admin)/src/main/java/com/example/study/model/SearchParam.java
@@ -1,32 +1,14 @@
 package com.example.study.model;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class SearchParam {
     private String account;
     private String email;
     private  int page;
 
-    public void setAccount(String account) {
-        this.account = account;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPage(int page) {
-        this.page = page;
-    }
-
-    public String getAccount() {
-        return account;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public int getPage() {
-        return page;
-    }
 }
