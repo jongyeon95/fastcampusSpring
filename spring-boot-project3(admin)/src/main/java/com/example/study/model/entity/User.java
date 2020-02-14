6054260d0bcdeb6540747fe3ccec2bf30587ba7e
whddus9509 <whddus9509@naver.com>
2020-02-14 13:29:37 +0900

    add jpaRepository, model user

---
+++ spring-boot-project3(admin)/src/main/java/com/example/study/model/entity/User.java
@@ -0,0 +1,29 @@
+package com.example.study.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String account;
+    private String email;
+    private String phoneNumber;
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime updatedAt;
+    private String updatedBy;
+}
