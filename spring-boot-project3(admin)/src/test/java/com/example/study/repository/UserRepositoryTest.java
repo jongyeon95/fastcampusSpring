13ccc6200c52345cf00233190a0a0f2413214037
whddus9509 <whddus9509@naver.com>
2020-02-14 14:25:46 +0900

    Update UserRepositoryTest.java
    Update read() and update()

--- spring-boot-project3(admin)/src/test/java/com/example/study/repository/UserRepositoryTest.java
+++ spring-boot-project3(admin)/src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -5,7 +5,9 @@ import com.example.study.model.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.swing.text.html.Option;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 public class UserRepositoryTest extends StudyApplicationTests {
 
@@ -23,7 +25,27 @@ public class UserRepositoryTest extends StudyApplicationTests {
         User newUser=userRepository.save(user);
         System.out.println(newUser);
     }
-    public void read(){}
-    public void update(){}
+    @Test
+    public void read(){
+        Optional<User> user=userRepository.findById(2L);
+
+        user.ifPresent(selectUser ->{
+            System.out.println("user : "+selectUser);
+            System.out.println("email :"+selectUser.getEmail());
+        });
+
+    }
+
+    @Test
+    public void update(){
+        Optional<User> user=userRepository.findById(2L);
+        user.ifPresent(selectUser ->{
+            selectUser.setAccount("pppp");
+            selectUser.setUpdatedAt(LocalDateTime.now());
+            selectUser.setUpdatedBy("update method()");
+            userRepository.save(selectUser);
+        });
+
+    }
     public void delete(){}
 }
