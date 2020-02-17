5703f382d74f74feb05d10e3876173d5489f2ddb
whddus9509 <whddus9509@naver.com>
2020-02-17 13:31:59 +0900

    update Repositorys

--- spring-boot-project3(admin)/src/main/java/com/example/study/model/entity/AdminUser.java
+++ spring-boot-project3(admin)/src/main/java/com/example/study/model/entity/AdminUser.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public class Admin {
+public class AdminUser {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- spring-boot-project3(admin)/src/main/java/com/example/study/repository/PartnerRepository.java
+++ spring-boot-project3(admin)/src/main/java/com/example/study/repository/PartnerRepository.java
@@ -1,13 +1,9 @@
 package com.example.study.repository;
 
-import com.example.study.model.entity.User;
+import com.example.study.model.entity.Partner;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface UserRepository extends JpaRepository<User,Long> {
-
-
+public interface PartnerRepository extends JpaRepository<Partner,Long> {
 }
