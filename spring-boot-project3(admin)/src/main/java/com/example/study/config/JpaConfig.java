059514ee2050851b1aa24edf49fb8277e1ce2795
whddus9509 <whddus9509@naver.com>
2020-02-20 19:59:23 +0900

    create config, componet

---
+++ spring-boot-project3(admin)/src/main/java/com/example/study/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package com.example.study.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing // JPA 감시 활성화
+public class JpaConfig {
+
+}
