38c95bd846280d7576b4d578cff13d630fb5cf74
whddus9509 <whddus9509@naver.com>
2020-02-22 17:00:33 +0900

    Update

--- spring-boot-project3(admin)/src/main/java/com/example/study/controller/api/UserApiController.java
+++ spring-boot-project3(admin)/src/main/java/com/example/study/controller/api/UserApiController.java
@@ -3,28 +3,37 @@ package com.example.study.controller.api;
 
 import com.example.study.ifs.CrudInterface;
 import com.example.study.model.network.Header;
+import com.example.study.model.network.request.UserApiRequest;
+import com.example.study.model.network.response.UserApiResponse;
+import com.example.study.service.UserApiLogicService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/user")
-public class UserApiController implements CrudInterface {
+public class UserApiController implements CrudInterface<UserApiRequest, UserApiResponse> {
 
+    @Autowired
+    private UserApiLogicService userApiLogicService;
 
     @Override
     @PostMapping("")
-    public Header create() {
-        return null;
+    public Header<UserApiResponse> create(@RequestBody Header<UserApiRequest> userApiRequest) {
+        log.info("{}",userApiRequest);
+        return userApiLogicService.create(userApiRequest);
     }
 
     @Override
     @GetMapping("{id}") //api//user/{id}
-    public Header read(@PathVariable(name = "id") Long id) {
+    public Header<UserApiResponse> read(@PathVariable(name = "id") Long id) {
         return null;
     }
 
     @Override
     @PutMapping("")
-    public Header update() {
+    public Header<UserApiResponse> update(@RequestBody Header<UserApiRequest> userApiRequest) {
         return null;
     }
 
--- spring-boot-project3(admin)/src/main/java/com/example/study/ifs/CrudInterface.java
+++ spring-boot-project3(admin)/src/main/java/com/example/study/ifs/CrudInterface.java
@@ -2,13 +2,13 @@ package com.example.study.ifs;
 
 import com.example.study.model.network.Header;
 
-public interface CrudInterface {
+public interface CrudInterface<Req,Res> {
 
-    Header create(); // todo requset 추가
+    Header<Res> create(Header<Req> request); // todo requset 추가
 
-    Header read(Long id);
+    Header<Res> read(Long id);
 
-    Header update();
+    Header<Res> update(Header<Req> request);
 
     Header delete(Long id);
 }
