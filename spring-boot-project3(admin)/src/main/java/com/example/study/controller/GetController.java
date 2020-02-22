3b06f6b56f1bdca5da08db60e779628012e970b0
whddus9509 <whddus9509@naver.com>
2020-02-22 16:06:01 +0900

    update

--- spring-boot-project3(admin)/src/main/java/com/example/study/controller/GetController.java
+++ spring-boot-project3(admin)/src/main/java/com/example/study/controller/GetController.java
@@ -1,6 +1,7 @@
 package com.example.study.controller;
 
 import com.example.study.model.SearchParam;
+import com.example.study.model.network.Header;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -23,5 +24,10 @@ public class GetController {
         System.out.println(searchParam.getEmail());
         System.out.println(searchParam.getPage());
         return searchParam;
+    }
+
+    @GetMapping("/Header")
+    public Header getHeader() {
+        return Header.builder().resultCode("OK").description("OK").build();
     }
 }
