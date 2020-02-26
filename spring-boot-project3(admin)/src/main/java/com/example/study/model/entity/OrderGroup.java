fd4c3e439973793fbb7d2b4a359ef0a6ff7a8709
whddus9509 <whddus9509@naver.com>
2020-02-26 23:57:59 +0900

    Update OrderType Enum

--- spring-boot-project3(admin)/src/main/java/com/example/study/model/entity/OrderGroup.java
+++ spring-boot-project3(admin)/src/main/java/com/example/study/model/entity/OrderGroup.java
@@ -1,6 +1,7 @@
 package com.example.study.model.entity;
 
 
+import com.example.study.model.enumClass.OrderType;
 import lombok.*;
 import lombok.experimental.Accessors;
 import org.springframework.data.annotation.CreatedBy;
@@ -30,7 +31,8 @@ public class OrderGroup {
 
     private String status;
 
-    private String orderType;
+    @Enumerated(EnumType.STRING)
+    private OrderType orderType;
 
     private String revAddress;
 
